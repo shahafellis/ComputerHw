a2a9055de5736dc5d6b0a19ca5659881e74ef8b1
shahafellis <amziki123@gmail.com>
2020-06-22 11:06:45 +0300

    Add project files.

---
+++ Computer1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerHw
+{
+    [DebuggerDisplay("{_model}")]
+
+    class Computer1
+    {
+
+        public string _model;
+
+        public int _price;
+
+        public int _numberOfProcessors;
+
+        public float _screenSize;
+
+        public bool _isTurnOn;
+
+        public int tellMeThePrice()
+        {
+            return _price;
+        }
+
+        public float tellMeTheScreenSize()
+        {
+            return _screenSize;
+        }
+
+        public bool turnOnOrOff()
+        {
+            if (_isTurnOn == true)
+
+                Console.WriteLine($"The coumputer is turn on");
+
+            else
+            {
+                Console.WriteLine($"The coumputer is turn off");
+            }
+
+            return _isTurnOn;
+        }
+
+        public int addProcessor()
+
+        {
+            return _numberOfProcessors;
+        }
+
+        public override string ToString()
+        {
+            return $"Class computer mode {_model} price = {_price} number of processors = {_numberOfProcessors} screen size = {_screenSize} the computer is on or off ? {turnOnOrOff()} ";
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerHw
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Computer1 Dell = new Computer1()
+            {
+                _model = "Dell",
+
+                _price = 8000,
+
+                _numberOfProcessors = 5,
+
+                _isTurnOn = true,
+
+                _screenSize = 32
+
+
+            };
+
+            Computer1 HP = new Computer1()
+            {
+                _model = "HP",
+
+                _price = 5000,
+
+                _numberOfProcessors = 4,
+
+                _isTurnOn = false,
+
+                _screenSize = 28
+            };
+
+            Computer1 Lenovo = new Computer1()
+            {
+
+                _model = "Lenovo",
+
+                _price = 10000,
+
+                _numberOfProcessors = 6,
+
+                _isTurnOn = true,
+
+                _screenSize = 34
+            };
+
+
+
+            Console.WriteLine(Dell);
+
+
+
+
+
+        }
+    }
+}
